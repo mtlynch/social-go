324f70fc79bdb8021bf16cbfe3479c04bac923dc
Michael Lynch <mtlynch@users.noreply.github.com>
2022-02-13 09:35:44 -0500

    Add more documentation (#4)

---
+++ social/examples_test.go
@@ -0,0 +1,45 @@
+package social_test
+
+import (
+	"fmt"
+
+	"github.com/mtlynch/social-go/v2/social"
+)
+
+func Example_facebook() {
+	raw := "zuck"
+	fb, _ := social.ParseFacebookUsername(raw)
+	fmt.Printf("%s -> %s\n", raw, fb)
+
+	raw = "https://facebook.com/zuck"
+	fb2, _ := social.ParseFacebookUsername(raw)
+	fmt.Printf("%s -> %s\n", raw, fb2)
+}
+
+func Example_twitter() {
+	raw := "jack"
+	t1, _ := social.ParseTwitterHandle(raw)
+	fmt.Printf("%s -> %s\n", raw, t1)
+
+	raw = "@jack"
+	t2, _ := social.ParseTwitterHandle(raw)
+	fmt.Printf("%s -> %s\n", raw, t2)
+
+	raw = "twitter.com/@jack"
+	t3, _ := social.ParseTwitterHandle(raw)
+	fmt.Printf("%s -> %s\n", raw, t3)
+}
+
+func Example_instagram() {
+	raw := "chelseahandler"
+	h1, _ := social.ParseInstagramHandle(raw)
+	fmt.Printf("%s -> %s\n", raw, h1)
+
+	raw = "@chelseahandler"
+	h2, _ := social.ParseInstagramHandle(raw)
+	fmt.Printf("%s -> %s\n", raw, h2)
+
+	raw = "instagram.com/@chelseahandler"
+	h3, _ := social.ParseInstagramHandle(raw)
+	fmt.Printf("%s -> %s\n", raw, h3)
+}
